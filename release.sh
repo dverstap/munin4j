31b9116eb6ae6ada16c9689b867e196b8e421d94
Davy Verstappen <davy.verstappen@gmail.com>
2011-02-27 19:18:07 +0100

    setting up for release builds

---
+++ release.sh
@@ -0,0 +1,24 @@
+#/usr/bin/env bash
+
+set -e
+
+# See this to understand how this is supposed to work:
+# http://cemerick.com/2010/08/24/hosting-maven-repos-on-github/
+
+mvn --batch-mode release:prepare
+
+# the release build will actually run in target/checkout,
+# so let's clone the maven repo at target/checkout/../maven-repo :
+cd target
+git clone git@github.com:dverstap/maven-repo.git
+cd ..
+
+mvn -DaltDeploymentRepository=dverstap-github-releases::default::file:../maven-repo/maven2/releases release:perform
+
+cd target/maven-repo
+git add .
+git commit -a -m "New releases"
+git push origin master
+cd ../..
+
+git fetch
